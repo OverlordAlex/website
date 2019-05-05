e0f77c0893659e42d40d42aa38ff51e3922509f9
Alex Wright <AlexParadiseWright+git@gmail.com>
2019-05-05 16:36:42 +0200

    Added a references section

--- build.sh
+++ build.sh
@@ -11,10 +11,16 @@ cp -r src/* out/
 rm -f interp/99_NOW
 echo "<time>"$(date)"</time>" > interp/99_NOW
 
+# do all the file templates
 for f in interp/*; do
     filename=$(basename "$f" | cut -c4-)
     find out/ -type f -exec sed -e "/##$filename##/  {" -e "r $f" -e "d" -e "}" -i {} \;
 done
+
+# do the inline citations
+find out/ -type f -exec sed -i -r "s/##\^(\[[[:digit:]]+\])##/<sup><a href=\"#citation\1\">\1<\/a><\/sup>/g" {} \;
+# do the citaiton section
+find out/ -type f -exec sed -i -r "s/##(\[[[:digit:]]+\])(.+)##/<tr id=\"citation\1\"><td align=\"right\">\1<\/td><td>\2<\/td><\/tr>/g" {} \;
 
 # find the most recent blog post and make that the home page
 recent=$(find out/blog/*.html -type f -printf '%T@ %p\n' | sort -n | tail -1 | cut -f2- -d" ")
