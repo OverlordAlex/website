495b3e300bd8df4e0e2282e98ca8ba6f50d01b2a
Alex Wright <AlexParadiseWright+git@gmail.com>
2019-05-05 21:55:40 +0200

    Added blog links to sidebar

--- build.sh
+++ build.sh
@@ -11,6 +11,16 @@ cp -r src/* out/
 rm -f interp/99_NOW
 echo "<time>"$(date)"</time>" > interp/99_NOW
 
+# generate a list of the last 5 blog posts
+# TODO: work out how to template in without changing mtime
+rm -f interp/99_POST_LIST
+latest=$(find out/blog/*.html -type f -printf '%T@ %p\n' | sort -n | tail -5 | cut -f2- -d" ")
+for f in "$latest"; do
+    title=$(grep -A1 "class=\"heading" "$f" | grep -oP "h1>\K(.+)(?=<)")
+    url=$(echo "$f" | cut -f2- -d"/")
+    echo "<li><a href=\"/$url\">$title</a></li>" >> interp/99_POST_LIST
+done
+
 # do all the file templates
 for f in interp/*; do
     filename=$(basename "$f" | cut -c4-)
@@ -30,3 +40,4 @@ cp "$recent" out/index.html
 rm -f out/sitemap*
 tree -I 'sitemap*' -H '' -C -U -P "*html" --prune --noreport out/ -o out/sitemap.html
 tree -I 'sitemap*' -X    -C -U -P "*html" --prune --noreport out/ -o out/sitemap.xml
+
